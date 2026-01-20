0546ba12cb0d3b7186c8563ab7756f5812fa6d36
Mihai <mieitza@users.noreply.github.com>
2026-01-20 10:44:59 +0200

    Update default project ID in sanity.cli.ts

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -2,7 +2,7 @@ import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.SANITY_STUDIO_PROJECT_ID || "your-project-id",
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID || "jrklhir1",
     dataset: process.env.SANITY_STUDIO_DATASET || "production",
   },
 });
